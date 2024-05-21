3bbe6c4aa41150f35e3cebc0a077d78b2af34756
Nikhar Waris <mohnikharwaris@gmail.com>
2024-05-21 21:46:36 +0530

    new code

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-const heading = React.createElement("h1", {}, "Hello World From React!");
+const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World From React!");
     // this API need three parameter, first-what element need to create, second-Object, third-what you should show in the element
 
+    console.log(heading);//returns an object
     const root = ReactDOM.createRoot(document.getElementById("root"));//ReatDom Library to make root 
 
-    root.render(heading);
+    root.render(heading);//convert this object in heading and put it back in ReactDM.createRoot
